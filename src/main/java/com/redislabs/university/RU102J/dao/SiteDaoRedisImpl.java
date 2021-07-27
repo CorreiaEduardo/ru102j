6e9bac2ee64b1cdc778279754fd323a5327a33f9
Eduardo Correia <correiaeduardojr@gmail.com>
2021-07-27 10:51:50 -0300

    feat: add challenge one solution

--- src/main/java/com/redislabs/university/RU102J/dao/SiteDaoRedisImpl.java
+++ src/main/java/com/redislabs/university/RU102J/dao/SiteDaoRedisImpl.java
@@ -27,8 +27,28 @@ public class SiteDaoRedisImpl implements SiteDao {
 
     @Override
     public Site findById(long id) {
+        String key = RedisSchema.getSiteHashKey(id);
+        return parseSite(key);
+    }
+
+    // Challenge #1
+    @Override
+    public Set<Site> findAll() {
+        // START Challenge #1
+        Set<Site> result = new HashSet<>();
+        try (Jedis jedis = jedisPool.getResource()) {
+            final Set<String> siteIds = jedis.smembers(RedisSchema.getSiteIDsKey());
+            siteIds.forEach(siteId -> {
+                final Site site = parseSite(siteId);
+                result.add(site);
+            });
+        }
+        return result;
+        // END Challenge #1
+    }
+
+    private Site parseSite(String key) {
         try(Jedis jedis = jedisPool.getResource()) {
-            String key = RedisSchema.getSiteHashKey(id);
             Map<String, String> fields = jedis.hgetAll(key);
             if (fields == null || fields.isEmpty()) {
                 return null;
@@ -36,13 +56,5 @@ public class SiteDaoRedisImpl implements SiteDao {
                 return new Site(fields);
             }
         }
-    }
-
-    // Challenge #1
-    @Override
-    public Set<Site> findAll() {
-        // START Challenge #1
-        return Collections.emptySet();
-        // END Challenge #1
     }
 }
